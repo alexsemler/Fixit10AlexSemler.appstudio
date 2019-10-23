40b4402f82e13d3bb702a682711f2986cc2c1e4a
alexsemler <alex.semler03@gmail.com>
2019-10-23 10:51:17 -0500

    V1

---
+++ forms/customerSelect/customerSelect.js
@@ -0,0 +1,24 @@
+
+checkBtn.onclick=function(){
+    let choice = choiceInput.value
+    let query = "SELECT * FROM customer WHERE state = " + '"' + choice + '"'  
+
+    req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ajs85167&pass=BIA375&database=ajs85167&query=" + query)
+
+    if (req1.status == 200) { //transit worked.
+        results = JSON.parse(req1.responseText)
+        console.log(results)
+    if (results.length == 0)
+        NSB.MsgBox("There are no pets of that type.")
+    else {        
+        // output the names of all the dogs
+        var message = ""
+        for (i = 0; i <= results.length - 1; i++)
+            message = message + results[i][1] + "\n"
+        choiceOutput.value = message
+      } 
+
+  } else
+        //transit error - Handle that with an error message.
+        NSB.MsgBox("Error code: " + req1.status)
+}
